26bf13f7bbc175145af10b1113d09facb64acef4
Michael Tichopad <michael@tichopad.dev>
2025-11-02 23:22:46 +0100

    Added Tailwind + removed most tutorial stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  adapter: cloudflare(),
+  adapter: cloudflare({ imageService: "compile" }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,12 +1,19 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
 import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
 
 export const GET: APIRoute = async (context) => {
+  const posts = await getCollection("blog");
   return rss({
     title: "Astro Learner | Blog",
     description: "My journey learning Astro",
     site: context.site ?? "",
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.id}/`,
+    })),
     customData: `<language>en-us</language>`,
   });
 };
